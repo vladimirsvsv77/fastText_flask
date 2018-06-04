b5de672f2c01ad1153b9d96eebc801ebb5a92cd9
vladimirsvsv77 <vladimirsvsv77@gmail.com>
2018-06-04 10:53:46 +0300

    added integer check

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ __author__ = 'VladimirSveshnikov'
 import gensim
 from scipy.spatial.distance import euclidean
 import string
+import inflect
 
 from flask import Flask, request, jsonify
 
@@ -12,9 +13,10 @@ app = Flask(__name__)
 
 print('start app')
 # model link: https://fasttext.cc/docs/en/english-vectors.html
-model_path = 'your_path_to_model'
+model_path = '/mnt/data/crawl-300d-2M.vec'
 model_fastText = gensim.models.KeyedVectors.load_word2vec_format(model_path)
 print('model loaded')
+p = inflect.engine()
 
 
 def clean_str(s):
@@ -28,9 +30,13 @@ def get_similarity_euql(model, first_sentence, second_sentence):
     first_sentence = [i for i in clean_str(first_sentence).split() if i in model]
     second_sentence = [i for i in clean_str(second_sentence).split() if i in model]
     for i in first_sentence:
+        if i.isdigit():
+            i = p.number_to_words(int(i))
         first_vector = model[i]
         sim_i = 0
         for j in second_sentence:
+            if j.isdigit():
+                j = p.number_to_words(int(j))
             second_vector = model[j]
             sim_i += euclidean(first_vector, second_vector)
         similarity += sim_i / len(second_sentence)
@@ -40,8 +46,8 @@ def get_similarity_euql(model, first_sentence, second_sentence):
 @app.route("/get_dist", methods=["GET"])
 def api():
     if request.method == "GET":
-        s1 = request.args.get('s1')
-        s2 = request.args.get('s2')
+        s1 = clean_str(request.args.get('s1').lower())
+        s2 = clean_str(request.args.get('s2').lower())
         dist = get_similarity_euql(model_fastText, s1, s2)
 
         return jsonify({'dist': dist})
